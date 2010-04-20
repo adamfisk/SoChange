06992863bb258409a03d1b624d040ddd9bb3bfc6
Gleb Kuznetsov <glebk@glebk-laptop.(none)>
2010-04-20 03:08:42 -0400

    ported email capture part way, data still being sent to gleb.mit.edu

--- sochange/urls.py
+++ sochange/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^about', include('emailCapture.urls')), # temporary email collector
     (r'^users/', include('users.urls')),
     (r'^admin/', include(admin.site.urls)),
     #(r'^accounts/', include('registration.backends.simple.urls')), # transition to default?
