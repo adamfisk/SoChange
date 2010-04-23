adc02ab1495b14048b23e8da06a3267e601b3ca3
Gleb Kuznetsov <glebk@glebk-laptop.(none)>
2010-04-23 15:43:44 -0400

    still a noob with git, fix last commit

--- sochange/templates/emailCapture/writefile.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myFile = "testFile.txt";
-$fh = fopen($myFile, 'w') or die("can't open file");
-$stringData = "Bobby Bopper\n";
-fwrite($fh, $stringData);
-$stringData = "Tracy Tanner\n";
-fwrite($fh, $stringData);
-fclose($fh);
-?>
