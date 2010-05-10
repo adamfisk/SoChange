40c1fae4521b45120723b6b074f34c74cea9440f
afisk <a@littleshoot.org>
2010-05-10 09:52:13 -0700

    Initial campaigns REST layout

--- sochange/campaigns/urls.py
+++ sochange/campaigns/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('campaigns.views',
-    (r'^$', 'campaign_view'),
+    (r'^$', 'index'),
+    (r'^new/', 'new'),
+    (r'^edit', 'edit'),
+    (r'^show', 'show'),
 )
--- sochange/campaigns/views.py
+++ sochange/campaigns/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext
 
-def campaign_view(request):
-    return render_to_response('campaigns/campaign.html', context_instance=RequestContext(request))
+def index(request):
+    if request.method == 'POST':
+        return create(request)
+    else:
+        return render_to_response('campaigns/index.html', context_instance=RequestContext(request))    
+
+def create(request):
+    return render_to_response("campaigns/create.html")
+    
+def new(request):
+    return render_to_response("campaigns/new.html")
+    
+def show(request):
+    return render_to_response("campaigns/show.html")
+    
+def edit(request):
+    return render_to_response("campaigns/edit.html")
+    
