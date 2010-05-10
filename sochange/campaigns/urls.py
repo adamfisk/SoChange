c67afd55be4bbee48356c9d4e960185c5db8aa4c
afisk <a@littleshoot.org>
2010-05-10 10:10:22 -0700

    fixed a bit

--- sochange/campaigns/urls.py
+++ sochange/campaigns/urls.py
@@ -4,6 +4,6 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('campaigns.views',
     (r'^$', 'index'),
     (r'^new/', 'new'),
-    (r'^edit', 'edit'),
-    (r'^show', 'show'),
+    (r'^(?P<campaignId>\d+)/edit', 'edit'),
+    (r'^(?P<campaignId>\d+)', 'show'),
 )
--- sochange/campaigns/views.py
+++ sochange/campaigns/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext
 
+import logging
+
 def index(request):
     if request.method == 'POST':
         return create(request)
@@ -9,14 +11,16 @@ def index(request):
         return render_to_response('campaigns/index.html', context_instance=RequestContext(request))    
 
 def create(request):
-    return render_to_response("campaigns/create.html")
+    return render_to_response("campaigns/create.html", context_instance=RequestContext(request))
     
 def new(request):
-    return render_to_response("campaigns/new.html")
+    return render_to_response("campaigns/new.html", context_instance=RequestContext(request))
     
-def show(request):
-    return render_to_response("campaigns/show.html")
+def show(request, campaignId):
+    logging.info("Campaign ID: %s", campaignId)
+    return render_to_response("campaigns/show.html", context_instance=RequestContext(request))
     
-def edit(request):
-    return render_to_response("campaigns/edit.html")
+def edit(request, campaignId):
+    logging.info("Campaign ID: %s", campaignId)
+    return render_to_response("campaigns/edit.html", context_instance=RequestContext(request))
     
